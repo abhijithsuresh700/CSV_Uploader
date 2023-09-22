739043c944ce9fc6021790af8625ed936bbcf673
Abhijith Suresh <abhijithsuresh700@gmail.com>
2023-09-22 18:56:56 +0530

    change in baseURL

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000/api",
-  baseURL: "https://csv-uploader1-tq05.onrender.com",
+  baseURL: "https://csv-uploader1-tq05.onrender.com/api",
 });
